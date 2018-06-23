88cd69afc70fc421bb98a99df4f39e9dd9591ea2
BobCatC <TwoFourSixEight@yandex.ru>
2018-06-23 14:58:43 +0300

    cosmetic changes

--- main.cpp
+++ main.cpp
@@ -1,84 +1,38 @@
-#include "CBsT.hpp"
+#include <string>
 #include <iostream>
-#include <sys/time.h>
+
+
+#include "CBsT.hpp"
 using namespace std;
 
 int main(){
-    CBsT<double, greater<int>> bst;
-    for(auto itPreorderDF = bst.beginPreorderDF(); itPreorderDF != bst.end(); ++itPreorderDF){
-        cout << *itPreorderDF << ' ';
-    }
-    bst.insert(5);
-    bst.insert(3);
-    bst.insert(4);
-    bst.insert(2);
-    bst.insert(0);
-    bst.insert(1);
-    bst.insert(-1);
-    bst.insert(1.5);
-    bst.insert(33);
-    bst.insert(0.97);
-    bst.insert(1.25);
-    bst.insert(1.375);
-    bst.insert(-2);
-    bst.insert(-3);
-    bst.insert(-4);
-    bst.insert(-5);
-    bst.insert(-6);
-    bst.insert(-7);
-    bst.insert(-8);
-    bst.insert(6.5);
-    bst.insert(20);
-    bst.insert(15);
-    bst.insert(16);
-    bst.insert(19);
-    bst.insert(18);
-    bst.insert(14);
 	
-	for(auto it : bst){
-		cout << it << ' ';
+	CBsT<int, greater<int>> bst;
+	
+	srand(clock());
+	for(int i = 0; i < 20; ++i) {
+		bst.insert(rand());
 	}
-	cout << endl;
 	
-    cout << "Preorder REC:" << endl;
-    bst.printPreorderDF(bst.getRoot());
-    cout << endl << "Preorder my:" << endl;
-    
-    for(auto itPreorderDF = bst.beginPreorderDF(); itPreorderDF != bst.end(); ++itPreorderDF){
-        cout << *itPreorderDF << ' ';
-    }
+	for(auto it: bst) {
+		cout << it << endl;
+	}
 	
+	cout << endl << "Traversal Preorder Deep First" << endl;
+	for(auto it = bst.beginPreorderDF(); it != bst.end(); ++it) {
+		cout << *it << endl;
+	}
 	
-    
-    cout << endl << "Inorder REC" << endl;
-    bst.printInorderDF(bst.getRoot());
-    cout << endl << "Inorder my:" << endl;
-    for(auto itInorderDF = bst.beginInorderDF(); itInorderDF != bst.end(); ++itInorderDF){
-        cout << *itInorderDF << ' ';
-    }
-    
-    
-    cout << endl << "Postorder REC:" << endl;
-    bst.printPostorderDF(bst.getRoot());
-    
-    cout << endl << "Postorder my:" << endl;
-    for(auto itPostorderDF = bst.beginPostorderDF(); itPostorderDF != bst.end(); ++itPostorderDF){
-        cout << *itPostorderDF << ' ';
-    }
-    cout << endl;
-    
- 
-    cout << "Breadth-first my:" << endl;
-    for(auto itBF = bst.beginBF(); itBF != bst.end(); ++itBF){
-        cout << *itBF <<' ';
-    }
-    cout << endl << endl;
-   
-    for (auto it : bst) {
-        cout << it << ' ';
-    }
-    cout << endl;
-    
+	cout << endl << "Traversal Inorder Deep First" << endl;
+	for(auto it = bst.beginInorderDF(); it != bst.end(); ++it) {
+		cout << *it << endl;
+	}
+	
+	cout << endl << "Traversal Postorder Deep First" << endl;
+	for(auto it = bst.beginPostorderDF(); it != bst.end(); ++it) {
+		cout << *it << endl;
+	}
+	
 	bst.clear();
 	bst.insert(1);
 	bst.remove(1);
@@ -87,10 +41,10 @@ int main(){
 	bst.beginInorderDF();
 	bst.beginPostorderDF();
 	bst.beginPreorderDF();
+	bst.end();
 	bst.empty();
 	bst.size();
 	bst.find(1);
-	bst.getRoot();
 	
     return 0;
 }
